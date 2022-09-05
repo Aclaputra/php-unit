33748882f62e83ee50cf3e8db31635b0543233e5
Aclaputra <muhamadacla22@gmail.com>
2022-09-06 00:04:14 +0700

    fix: fixing UserServiceProvider typo

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -5,8 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Services\UserService;
 use App\Services\Impl\UserServiceImpl;
+use Illuminate\Contracts\Support\DeferrableProvider;
 
-class UserServiceProvider extends ServiceProvider
+class UserServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     public array $singletons = [
         UserService::class => UserServiceImpl::class
