9996e701f573e19d4efad2d024a0c77dc71f9324
Aclaputra <muhamadacla22@gmail.com>
2022-09-06 05:54:47 +0700

    feat: added remove todo service

--- app/Services/TodolistService.php
+++ app/Services/TodolistService.php
@@ -7,5 +7,7 @@ interface TodolistService
     public function saveTodo(string $id, string $todo): void;
 
     public function getTodoList(): array;
+
+    public function removeTodo(string $todoId);
 }
 
