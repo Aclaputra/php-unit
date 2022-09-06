4318b2784539bc98d6f9b40f23a53b89e53d91a9
Aclaputra <muhamadacla22@gmail.com>
2022-09-06 07:51:30 +0700

    feat: added test for add & remove todo

--- tests/Feature/TodolistControllerTest.php
+++ tests/Feature/TodolistControllerTest.php
@@ -27,4 +27,37 @@ class TodolistControllerTest extends TestCase
             ->assertSeeText("2")
             ->assertSeeText("putra");
     }
+
+    public function testAddTodoFailed() {
+        $this->withSession([
+            "user" => "putra"
+        ])->post("/todolist", [])
+            ->assertSeeText("Todo is required");
+    }
+
+    public function testAddTodoSuccess() {
+        $this->withSession([
+            "user" => "putra"
+        ])->post("/todolist", [
+            "todo" => "acla"
+        ])->assertRedirect("/todolist");
+    }
+
+    public function testRemoveTodolist() {
+        $this->withSession([
+            "user" => "putra",
+            "todolist" => [
+                [
+                  "id" => "1",
+                  "todo" => "acla"
+                ],
+                [
+                  "id" => "2",
+                  "todo" => "putra"
+                ]
+             ]
+        ])->post("/todolist/1/delete")
+            ->assertRedirect("/todolist");
+
+    }
 }
